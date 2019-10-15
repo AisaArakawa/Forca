6be8a9198fef501b4b310a59ad8d871feac8a102
AisaArakawa <48520755+AisaArakawa@users.noreply.github.com>
2019-10-15 14:55:17 -0300

    Forca 0
    Jogo da forca versão 0

--- Jogo da forca/funcoes.c
+++ Jogo da forca/funcoes.c
@@ -4,6 +4,13 @@
 #include <string.h>
 //Victor Hugo Soares Pereira 17137 Ana Iara Silva Arakawa 18120
 
+struct noSecreto{
+    int status;
+    char palavra [31];
+    char assunto [100];
+    struct noSecreto * prox;
+};
+
 NoSecreto * inicializaListaSecreta()
 {
     return NULL;
@@ -11,34 +18,23 @@ NoSecreto * inicializaListaSecreta()
 
 NoSecreto * carregaListaArquivo(NoSecreto * l, char nomeArq[255])
 {
-    const char ch[2]=";";
-    char * token;
     int status;
-    char palavra[31];
-    char assunto[101];
-    char * linha[1024];
+    char palavra [31];
+    char assunto [100];
     FILE * fWords;
-
     fWords = fopen(nomeArq,"r");
-    if(fWords==NULL){
-        printf("Falha ao acessar base de dados!!!\n\n");
+    if (fWords==NULL)
+    {
+        printf("Falha ao acessar a base de dados!!!\n\n");
         exit(0);
     }
-    while(fgets(linha,1024,fWords))
+    while(!feof(fWords))
     {
-        token = strtok(linha,ch);
-        if(token!=NULL)
+        if(fscanf(fWords,"%s%s", palavra, assunto))
         {
-             strcpy(palavra,token);
+            l = inserePalavraSecreta(l, palavra, assunto);
         }
-        token=strtok(NULL,ch);
-        if(token!=NULL)
-        {
-            strcpy(assunto,token);
-        }
-        l = inserePalavraSecreta(l,palavra, assunto);
     }
-
     fclose(fWords);
     return l;
 }
@@ -61,61 +57,6 @@ void imprimeListaSecreta(NoSecreto *l)
     {
         printf("%0d | %30s | %30s\n", p->status, p->palavra, p->assunto);
     }
-}
-
-int geraAleatorio(int maximo)
-{
-    time_t t;
-    int n=0;
-    srand((unsigned) time(&t));
-    n = (rand() % maximo)+1;
-    return n;
-}
-
-NoSecreto * sorteiaPalavra(NoSecreto *l)
-{
-    int tam = tamanhoListaSecreta(l);
-    int aleatorio=0;
-    int tentativas = 0;
-    NoSecreto * p;
-
-    while(temPalavraNaoUsada(l)){
-        aleatorio = geraAleatorio(tam);
-        p = retornaPalavraPos(l,aleatorio);
-        if(p->status==0){
-            p->status = 1;
-            return p;
-        }
-    }
-    return NULL;
-}
-
-int tamanhoListaSecreta(NoSecreto *l)
-{
-    int n=0;
-    for(NoSecreto *p=l;p!=NULL;p=p->prox,n++);
-    return n;
-}
-
-int temPalavraNaoUsada(NoSecreto *l)
-{
-    NoSecreto *p;
-    for(p=l;p!=NULL;p=p->prox){
-        if(p->status==0) return 1;
-    }
-    return 0;
-}
-
-NoSecreto * retornaPalavraPos(NoSecreto *l, int pos)
-{
-    int n=1;
-    NoSecreto *p;
-    for(p=l; p!=NULL; p=p->prox)
-    {
-        if(n==pos) break;
-        n++;
-    }
-    return p;
 }
 
 
